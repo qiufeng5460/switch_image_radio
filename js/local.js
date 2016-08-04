c48c8e244a99e3c0ee7fc261fa97169ede86f81a
qiufeng5460 <qiufeng5460@sohu.com>
2016-08-04 17:17:26 +0800

    add JS function&file

---
+++ js/local.js
@@ -0,0 +1,39 @@
+﻿
+       function button_click(i)
+	   {
+	   	    var img_src = "img/"+i+".jpg";		  
+//           slide mode
+			 $("#main_image").animate({"left":-410},"slow",function(){
+			   $("#main_image").attr("src",img_src);
+			   $("#main_image").css({"left":410}); 
+               $("#main_image").animate({"left":0},"slow");			   
+			});
+			//set radio&p&button 
+			$('input').iCheck('uncheck');
+			$('input').iCheck('enable');
+			$('p#answer').css("visibility","hidden");
+			$('#next_button').attr("disabled","disabled");
+		  
+/*          fade mode
+		     $("#main_image").fadeOut("5000",function(){
+			    $("#main_image").attr("src",img_src);
+				setTimeout('$("#main_image").fadeIn("5000")',1000);
+			 });
+		  }); 
+*/
+	   
+	   }
+	   
+	   function set_anwser()
+	   {
+	      	  //set p
+			 if ("pic1_1" == $("input[type='radio']:checked").val())
+			 {
+			    $('#right_wrong').text("正确");
+			 }
+			 else
+			 {
+			    $('#right_wrong').text("错误");
+			 }
+			 $('p#answer').css("visibility","visible");   
+	   }
